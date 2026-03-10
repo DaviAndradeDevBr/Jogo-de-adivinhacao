374533c2753df62fb2333dabd347d09caabd441b
Davi Andrade <contatodaviandrade20@gmail.com>
2026-03-09 22:36:43 -0300

    Adicionada validação de entrada e lógica de repetição

--- Jogo de adivinhacao.ConsoleApp1/Program.cs
+++ Jogo de adivinhacao.ConsoleApp1/Program.cs
@@ -3,13 +3,59 @@
 // 2. Nosso jogo deve gerar um numero secreto aleatorio
 // 3. Nosso jogo deve validar a tentativa do jogador e exibir uma mensagem
 
-Console.WriteLine("------------------------------------------");
-Console.WriteLine("Jogo de adivinhaçao");
-Console.WriteLine("------------------------------------------");
+using System;
+using System.Security.Cryptography;
 
-Console.Write("Digite um número de 1 a 20: ");
-string? chute = Console.ReadLine();
+while (true == true)
+{
+    Console.Clear();
 
-Console.WriteLine("O valor digitado foi: " + chute);
+    Console.WriteLine("------------------------------------------");
+    Console.WriteLine("Jogo de Adivinhação");
+    Console.WriteLine("------------------------------------------");
 
-Console.ReadLine();
+    int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
+
+    Console.Write("Informe um número de 1 a 20: ");
+    string? chute = Console.ReadLine();
+
+
+    int numeroDigitado = Convert.ToInt32(chute);
+
+    if (numeroDigitado == numeroAleatorio)
+    {
+        Console.WriteLine("\n------------------------------------------");
+        Console.WriteLine("Parabéns, você acertou!");
+        Console.WriteLine("------------------------------------------");
+    }
+
+    else if (numeroDigitado > numeroAleatorio)
+    {
+        Console.WriteLine("\n------------------------------------------");
+        Console.WriteLine("O número digitado é maior que o número secreto.");
+        Console.WriteLine("------------------------------------------");
+    }
+
+    else
+    {
+        Console.WriteLine("\n------------------------------------------");
+        Console.WriteLine("O número digitado é menor que o número secreto.");
+        Console.WriteLine("------------------------------------------");
+    }
+
+
+    Console.WriteLine("\nPressione ENTER para continuar...");
+    Console.ReadLine();
+
+    Console.Write("\nDeseja continuar? (S/N): ");
+    string? opcaoContinuar = Console.ReadLine();
+
+    if (opcaoContinuar?.ToUpper() != "S")
+    {
+        break;
+    }
+
+    Console.WriteLine();
+}
+
+
